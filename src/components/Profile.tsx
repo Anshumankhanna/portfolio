5a99519cbb714cdc6caa01e3b0d5631c149f4572
Anshuman Khanna <anshumankh01@gmail.com>
2025-10-01 18:45:35 +0530

    Initital commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,15 @@
+export default function Profile() {
+	return (
+		<div>
+			<div>
+				Banner
+			</div>
+			<div>
+				Profile picture
+			</div>
+			<div>
+				Profile details
+			</div>
+		</div>
+	);
+}
