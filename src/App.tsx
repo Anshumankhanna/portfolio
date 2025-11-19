fcf754f64c06d346255d18a5641fffc0b8be2177
user.name like: "Anshuman Khanna <user.email like: "anshumankh01@gmail.com>
2025-11-19 13:59:28 +0530

    Add TODO(#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "@/App.css";
-
 import Navbar from "@/components/Navbar/Navbar";
 import Hero from "@/components/Hero/Hero";
 
 export default function App() {
+	// TODO(#3): In App.tsx JS part.
 	return <>
+		{/* TODO: In App.tsx TSX part. */}
 		<Navbar />
 		<main>
 			<Hero />
