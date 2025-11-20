133c75b881a8e299d53468bba9bbc4108903dd0b
user.name like: "Anshuman Khanna <user.email like: "anshumankh01@gmail.com>
2025-11-20 23:11:06 +0530

    Incomplete work on projects section

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,10 +4,20 @@ import projects from "@/utilities/projects-data";
 export default function Projects() {
 	return <section id="projects" className={style["projects__section"]}>
 		<h2>Projects</h2>
-		<ul>
-			{projects.length != 0 && projects.map((project, index) => (
-				<li key={index}>{project.title}</li>
+		<ol>
+			{projects.length != 0 && projects.map((project, projects_index) => (
+				<li key={projects_index}>
+					<hgroup className={style["project__heading"]}>
+						<h3>{project.title}</h3>
+						<h4>{project.title}</h4>
+					</hgroup>
+					<ul>
+						{project.description.length != 0 && project.description.map((point, description_index) => (
+							<li key={description_index}>{point}</li>
+						))}
+					</ul>
+				</li>
 			))}
-		</ul>
+		</ol>
 	</section>;
 };
