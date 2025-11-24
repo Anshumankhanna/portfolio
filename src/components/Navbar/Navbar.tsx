4eb7d8a06a2b4390fba52fbee853dbbe9b277d89
user.name like: "Anshuman Khanna <user.email like: "anshumankh01@gmail.com>
2025-11-25 00:31:45 +0530

    Resolved conflict because of main>section

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import styles from "@/components/Navbar/Navbar.module.css";
 export default function Navbar() {
 	return <>
 		{/* `nav` is here just for its sematnic meaning. */}
-		<nav>
+		<nav className={styles["nav"]}>
 			<ul className={styles["nav__list"]}>
 				<li><a href="/">Home</a></li>
 				<li><a href="#projects">Projects</a></li>
