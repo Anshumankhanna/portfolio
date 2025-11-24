a7be83efec7a13604607d8fc2ee2381e0c60d885
user.name like: "Anshuman Khanna <user.email like: "anshumankh01@gmail.com>
2025-11-25 00:22:27 +0530

    Commented unused vars in Skill

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,17 +1,17 @@
 import styles from "@/components/Skills/Skills.module.css";
-import type { ReactElement, SVGProps } from "react";
-
-type SkillType = {
-	icon: ReactElement<SVGProps<SVGSVGElement>>;
-	name: string;
-};
-
-function Skill({ icon, name }: SkillType) {
-	return <div>
-		{icon}
-		<span>{name}</span>
-	</div>
-}
+// import type { ReactElement, SVGProps } from "react";
+//
+// type SkillType = {
+// 	icon: ReactElement<SVGProps<SVGSVGElement>>;
+// 	name: string;
+// };
+//
+// function Skill({ icon, name }: SkillType) {
+// 	return <div>
+// 		{icon}
+// 		<span>{name}</span>
+// 	</div>
+// }
 
 export default function Skills() {
 	return <section className={styles["skills"]}>
