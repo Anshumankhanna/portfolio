7f2c17fc9535799c0b0dd2f80d6df25a981c8d74
user.name like: "Anshuman Khanna <user.email like: "anshumankh01@gmail.com>
2025-12-05 00:24:22 +0530

    Added icons in the commit

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,4 +1,6 @@
 import styles from "@/components/Skills/Skills.module.css";
+
+import htmlLogo from "@/assets/images/logo/html.svg";
 import tsLogo from "@/assets/images/logo/typescript.svg";
 import jsLogo from "@/assets/images/logo/javascript.svg";
 
@@ -7,7 +9,7 @@ function Skill({ href, icon, name }: {
 	icon: string;
 	name: string;
 }) {
-	return <a href={href} className={styles["skill"]}>
+	return <a href={href} target="_blank" className={styles["skill"]}>
 		<img src={icon} alt={name} />
 		<p>{name}</p>
 	</a>
@@ -20,15 +22,22 @@ export default function Skills() {
 			<li>
 				<Skill
 					href=""
-					icon={tsLogo}
-					name="typescript"
+					icon={htmlLogo}
+					name="HTML"
 				/>
 			</li>
 			<li>
 				<Skill
-					href=""
+					href="https://www.typescriptlang.org/"
+					icon={tsLogo}
+					name="TypeScript"
+				/>
+			</li>
+			<li>
+				<Skill
+					href="https://nodejs.org/en"
 					icon={jsLogo}
-					name="javascript"
+					name="JavaScript"
 				/>
 			</li>
 		</ul>
